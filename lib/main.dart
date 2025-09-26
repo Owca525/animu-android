b51fd6c7637c41e6a0edb821621eefa8d9483cd0
zomi <mikolajpietrzakowski3659@gmail.com>
2025-09-26 13:04:19 +0200

    add pages

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,75 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MainApp());
 }
+class Homepage extends StatefulWidget {
+   const Homepage({super.key});
 
+   @override
+  State<Homepage> createState() => _Homepage();
+}
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // Root widget
-      home: Scaffold(
-        body: FutureBuilder<List<AnimeData>>(
+      home: Homepage()
+    );
+  }
+}
+class _Homepage extends State<Homepage> {
+  int _currentIndex = 0;
+  List<Widget> pages = [
+    Home(),
+    AnimeList(),
+    History(),
+    Calendary(),
+    Settings()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home", backgroundColor: Color.fromARGB(255, 0, 0, 0), ),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: "AnimeList", backgroundColor: Color.fromARGB(255, 0, 0, 0)),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "History", backgroundColor: Color.fromARGB(255, 0, 0, 0)),
+          BottomNavigationBarItem(icon: Icon(Icons.event), label: "Calendary", backgroundColor: Color.fromARGB(255, 0, 0, 0)),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings", backgroundColor: Color.fromARGB(255, 0, 0, 0)),        
+        ],
+        selectedItemColor: Color.fromARGB(255, 255, 255, 255),
+        currentIndex: _currentIndex,
+        onTap: (index){
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      backgroundColor: Color.fromARGB(255, 228, 204, 99),
+    );
+  }
+} 
+class AnimeList extends StatelessWidget {
+  const AnimeList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<List<AnimeData>>(
           future: an_allTimePopularAnime(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -32,14 +91,45 @@ class MainApp extends StatelessWidget {
             }
           },
         ),
-        bottomNavigationBar: NavigationBar(destinations: [
-          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-          NavigationDestination(icon: Icon(Icons.list), label: "AnimeList"),
-          NavigationDestination(icon: Icon(Icons.history), label: "History"),
-          NavigationDestination(icon: Icon(Icons.event), label: "Calendary"),
-          NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
-        ]),
-      ),
     );
   }
-}
+} 
+class History extends StatelessWidget {
+  const History({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('History'),
+      ),
+      backgroundColor: Color.fromARGB(255, 18, 201, 110),
+    );
+  }
+} 
+class Calendary extends StatelessWidget {
+  const Calendary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendar'),
+      ),
+      backgroundColor: Color.fromARGB(255, 151, 52, 218),
+    );
+  }
+} 
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      backgroundColor: Color.fromARGB(255, 27, 221, 255),
+    );
+  }
+} 
